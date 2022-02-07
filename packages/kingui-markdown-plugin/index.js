25707129464cc3e7611cd31899b0f4eee70789a1
sunxinqiang <sunxinqiang@kingsoft.com>
2022-02-07 17:00:25 +0800

    🍔 重构: md plugin 提出单独包

---
+++ packages/kingui-markdown-plugin/index.js
@@ -0,0 +1,39 @@
+const fs = require('fs-extra')
+
+function mdLoader(code) {
+    code = code.replace(/"/g, '\'')
+    return `
+      <template>
+        <Markdown source="${code}" />
+      </template>
+      <script setup>
+        import Markdown from 'vue3-markdown-it';
+        import 'highlight.js/styles/github.css';
+      </script>
+    `
+}
+
+module.exports = function create (vuePlugin) {
+    return {
+        name: 'kingui-markdown-plugin',       // 手动实现一个 vite 插件将 .md 文件解析成 vue 文件
+        async transform(_, path) {
+            if (/\.(md)/.test(path)) {
+                const code = await fs.readFile(path, 'utf-8')
+                if (path.endsWith('.md')) {
+                    return mdLoader(code)
+                }
+            }
+        },
+        async handleHotUpdate(ctx) { // 热更新
+            const { file } = ctx
+            if (/\.(md)$/.test(file)) {
+                const code = await fs.readFile(file, 'utf-8')
+                let codeLoader = mdLoader(code)
+                return vuePlugin.handleHotUpdate({
+                    ...ctx,
+                    read: () => codeLoader
+                })
+            }
+        }
+    }
+}
